ae29240ee85efb9aab08e8a4632b116902982302
kathibr <kathix3@yahoo.de>
2013-09-11 21:48:44 +0200

    New Sample Page

--- src/de/projekt/viergewinnt/MainApp.java
+++ src/de/projekt/viergewinnt/MainApp.java
@@ -1,14 +1,62 @@
 package de.projekt.viergewinnt;
 
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import de.projekt.viergewinnt.view.*;
 
 public class MainApp extends Application {
 
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	  
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+		this.primaryStage = primaryStage;
+	    this.primaryStage.setTitle("die5gewinnt");
+	
+	
+	try {
+        // Load the root layout from the fxml file
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/RootLayout.fxml"));
+        rootLayout = (BorderPane) loader.load();
+        Scene scene = new Scene(rootLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    } catch (IOException e) {
+        // Exception gets thrown if the fxml file could not be loaded
+        e.printStackTrace();
+    }
+    
+    showSampleStage();
+	
 	}
-
+	
+	public Stage getPrimaryStage() {
+	      return primaryStage;
+	  }
+	
+	public void showSampleStage(){
+		try {
+	          // Load the fxml file and set into the center of the main layout
+	          FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/SampleStage.fxml"));
+	          AnchorPane overviewPage = (AnchorPane) loader.load();
+	          rootLayout.setCenter(overviewPage);
+	          
+	      } catch (IOException e) {
+	          // Exception gets thrown if the fxml file could not be loaded
+	          e.printStackTrace();
+	      }
+	}
+	
+	public static void main(String[] args) {
+	      launch(args);
+	  }
 }
