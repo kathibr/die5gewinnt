81666a08ef2e86f01dcbbd1e6174c0a8c3b2aa39
kathibr <kathix3@yahoo.de>
2013-09-11 21:33:35 +0200

    neues paket/klasse

---
+++ src/de/projekt/viergewinnt/MainApp.java
@@ -0,0 +1,14 @@
+package de.projekt.viergewinnt;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
