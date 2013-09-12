0f7ec6910ecd91ea53a0079726924e11a3d68734
kathibr <kathix3@yahoo.de>
2013-09-13 00:05:47 +0200

    erste screens zur einfachen anwendung des programms (achtung hässlich!!)

--- src/de/dhbw/die5gewinnt/MainApp.java
+++ src/de/dhbw/die5gewinnt/MainApp.java
@@ -6,59 +6,79 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import de.dhbw.die5gewinnt.view.*;
 
 public class MainApp extends Application {
-	//kathi4
-	//Arne
-	//nix daaa
-	//arnes test zum mergen
-	//blabla
 
 	private Stage primaryStage;
 	private BorderPane rootLayout;
 	  
+	public MainApp(){
+		
+	}
+	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) {
+			
 		this.primaryStage = primaryStage;
-	    this.primaryStage.setTitle("die5gewinnt");
+		  this.primaryStage.setTitle("die5gewinnt");
+		
+		  try {
+		    // Load the root layout from the fxml file
+		    FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/RootLayout.fxml"));
+		    rootLayout = (BorderPane) loader.load();
+		    Scene scene = new Scene(rootLayout);
+		    primaryStage.setScene(scene);
+
+		    // Give the controller access to the main app
+		    RootLayoutController controller = loader.getController();
+		    controller.setMainApp(this);
+
+		    primaryStage.show();
+		  } catch (IOException e) {
+		    // Exception gets thrown if the fxml file could not be loaded
+		    e.printStackTrace();
+		  }
+
+	}
 	
+	public void showPlayingField(){
+		try{
+			FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/PlayingField.fxml"));
+			AnchorPane playingFieldPage = (AnchorPane) loader.load();
+			rootLayout.setCenter(playingFieldPage);
+			
+			PlayingFieldController controller = loader.getController();
+			controller.setMainApp(this);
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+	}
 	
-	try {
-        // Load the root layout from the fxml file
-        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/RootLayout.fxml"));
-        rootLayout = (BorderPane) loader.load();
-        Scene scene = new Scene(rootLayout);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    } catch (IOException e) {
-        // Exception gets thrown if the fxml file could not be loaded
-        e.printStackTrace();
-    }
-    
-    showSampleStage();
-	
+	public void showOldGames(){
+		try{
+			FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/OldGames.fxml"));
+			AnchorPane oldGamesPage = (AnchorPane) loader.load();
+			rootLayout.setCenter(oldGamesPage);
+			
+			//muss implementiert werden sobald wir uns an die Ansicht alter Spiele machen			
+			//PlayingFieldController controller = loader.getController();
+			//controller.setMainApp(this);
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public Stage getPrimaryStage() {
 	      return primaryStage;
 	  }
-	
-	public void showSampleStage(){
-		try {
-	          // Load the fxml file and set into the center of the main layout
-	          FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/SampleStage.fxml"));
-	          AnchorPane overviewPage = (AnchorPane) loader.load();
-	          rootLayout.setCenter(overviewPage);
-	          
-	      } catch (IOException e) {
-	          // Exception gets thrown if the fxml file could not be loaded
-	          e.printStackTrace();
-	      }
-	}
 	
 	public static void main(String[] args) {
 	      launch(args);
