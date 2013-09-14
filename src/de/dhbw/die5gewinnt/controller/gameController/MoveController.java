1cf636ce57e7c37ccda330398f932c95155f6ee3
SysHannah <sun_ann@web.de>
2013-09-14 14:05:09 +0200

    neues Paket zur Übersichtlichen Spielsteuerung

---
+++ src/de/dhbw/die5gewinnt/controller/gameController/MoveController.java
@@ -0,0 +1,23 @@
+package de.dhbw.die5gewinnt.controller.gameController;
+
+public class MoveController {
+	
+	private int moveID;
+
+	
+	public void ownMove(){
+		
+	}
+	
+	
+	public void OpponentMove(){
+		
+	}
+	
+	
+	public void showMove(){
+		//Aufzeigen des Steines
+		moveID++;
+		
+	}
+}
