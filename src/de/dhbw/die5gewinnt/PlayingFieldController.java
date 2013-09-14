aa7d960da879c73488a138b5b4650c6088132576
kathibr <kathix3@yahoo.de>
2013-09-14 14:10:08 +0200

    css datei + änderung der farbe eines kreises

--- src/de/dhbw/die5gewinnt/PlayingFieldController.java
+++ src/de/dhbw/die5gewinnt/PlayingFieldController.java
@@ -2,10 +2,14 @@ package de.dhbw.die5gewinnt;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Dialogs;
+import javafx.scene.shape.Circle;
 
 public class PlayingFieldController {
 
 	private MainApp mainApp;
+	
+	@FXML
+	private Circle circle1;
 
 	public PlayingFieldController() {
 
@@ -22,9 +26,14 @@ public class PlayingFieldController {
 	
 	@FXML
 	public void handleClickMe(){
-		 Dialogs.showWarningDialog(mainApp.getPrimaryStage(),
-		          "Hallo Hallo",
-		          "Irgendein Schund", "Neuer Dialog");
+		// Dialogs.showWarningDialog(mainApp.getPrimaryStage(),
+		  //        "Hallo Hallo",
+		    //      "Irgendein Schund", "Neuer Dialog");
+		
+		circle1.getStyleClass().remove("emptyCircle");
+		circle1.getStyleClass().add("yellowCircle");
+		
+	
 	}
 	
 	@FXML
