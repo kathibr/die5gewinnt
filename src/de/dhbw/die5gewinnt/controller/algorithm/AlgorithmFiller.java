24a73ac5e5bc7b207079e185acaf1ff0136e0c51
Arne <Arne@141.72.134.113>
2013-09-26 09:51:35 +0200

    Algorithmus aufgeräumt, neue Klasse zum initialisieren der Arrays erstellt. ArrayList eingebaut um ungültige Kombinationen löschen zu können.

---
+++ src/de/dhbw/die5gewinnt/controller/algorithm/AlgorithmFiller.java
@@ -0,0 +1,117 @@
+package de.dhbw.die5gewinnt.controller.algorithm;
+
+import java.util.ArrayList;
+
+import de.dhbw.die5gewinnt.model.Move;
+import de.dhbw.die5gewinnt.model.Set;
+
+public class AlgorithmFiller {
+	public Move[][] field = new Move[7][6];
+	private Move[][] possibleCombinations = new Move[4][69];
+	private int row,collumn,start,threshold;
+	private String testcomment;
+	private ArrayList<Move[]> combinations = new ArrayList<Move[]>(69);
+	
+	public AlgorithmFiller(Set set){
+		field = set.getField();
+	}
+
+	public ArrayList<Move[]> fillList(){
+		fillVertical();
+		fillHorizontal();
+		fillDiagonal();
+		fillArrayList(possibleCombinations);
+		return combinations;
+	}
+	
+	public void fillVertical(){
+		System.out.println("FillVertical");
+		threshold = 2;
+		start = 5;
+		for(int i = 3; i > 0; i--){
+			for(int x = 6; x >= 0; x--){
+				collumn = 0;
+				testcomment = "";
+				for(int y = start; y >= threshold; y--){
+					possibleCombinations[collumn][row] = field[x][y];
+					collumn++;
+					testcomment = testcomment +" "+ x+""+y;
+				}
+				System.out.println(testcomment + " Zeile:"+row);
+				row++;
+			}
+			threshold--;
+			start--;
+		}
+	}
+	
+	public void fillHorizontal(){
+		System.out.println("fillHorizontal");
+		threshold = 3;
+		start = 6;
+		for(int i = 4; i > 0; i--){
+			for(int y = 5; y >= 0 ; y--){
+				collumn = 0;
+				testcomment = "";
+				for(int x = start; x >= threshold; x--){
+					possibleCombinations[collumn][row] = field[x][y];
+					collumn++;
+					testcomment = testcomment +" "+ x+""+y;
+				}
+				System.out.println(testcomment + " Zeile:"+row);
+				row++;
+			}
+			threshold--;
+			start--;
+		}
+	}
+	
+	public void fillDiagonal(){
+		System.out.println("FillDiagonal");
+		for(int y = 5; y >= 3; y--){
+			for(int x = 6; x >= 3; x--){
+				collumn = 0;
+				testcomment = "";
+				for(int i = 4; i > 0; i--){
+					possibleCombinations[collumn][row] = field[x][y];
+					collumn++;
+					testcomment = testcomment +" "+ x+""+y;
+					x--;
+					y--;
+				}
+				x = x + 4;
+				y = y + 4;
+				System.out.println(testcomment + " Zeile:"+row);
+				row++;
+			}
+		}
+		System.out.println("FillDiagonal2");
+		for(int y = 5; y >= 3; y--){
+			for(int x = 0; x <= 3; x++){
+				collumn = 0;
+				testcomment = "";
+				for(int i = 4; i > 0; i--){
+					possibleCombinations[collumn][row] = field[x][y];
+					collumn++;
+					testcomment = testcomment +" "+ x+""+y;
+					x++;
+					y--;
+				}
+				x = x - 4;
+				y = y + 4;
+				System.out.println(testcomment + " Zeile:"+row);
+				row++;
+			}
+		}
+	}
+	
+	public void fillArrayList(Move[][] array){
+		for(int i = 0; i < 69; i++){
+			Move[] row = new Move[4];
+			for(int y = 0; y < 4; y++){
+				row[y]=array[i][y];
+			}
+			combinations.add(i,row);
+		}
+	}
+}
--- src/de/dhbw/die5gewinnt/controller/algorithm/MastermindAlgorithm.java
+++ src/de/dhbw/die5gewinnt/controller/algorithm/MastermindAlgorithm.java
@@ -1,20 +1,21 @@
 package de.dhbw.die5gewinnt.controller.algorithm;
 
+import java.util.ArrayList;
+
 import de.dhbw.die5gewinnt.model.Move;
+import de.dhbw.die5gewinnt.model.Set;
 
 public class MastermindAlgorithm implements Runnable {
 
 	public int nextColumn = -1;
-	private Move[][] possibleCombinations;
 	public Move[][] field = new Move[7][6];
 	public int[] columnHeight = new int[7];
-	private int row,collumn,start,threshold;
-	private String testcomment;
+
+	private ArrayList<Move[]> possibleCombinations = new ArrayList<Move[]>(69);
 	
-	public MastermindAlgorithm(de.dhbw.die5gewinnt.model.Set set){
-		field = set.getField();
-		possibleCombinations = new Move[5][69];
-		fillField();
+	public MastermindAlgorithm(Set set){
+		AlgorithmFiller filler = new AlgorithmFiller(set);
+		possibleCombinations = filler.fillList();
 	}
 	
 	/* ALGORITHM-Interface */
@@ -25,93 +26,6 @@ public class MastermindAlgorithm implements Runnable {
 		}
 		System.out.println("MastermindAlgorithm: 8");
 		nextColumn = 8;
-	}
-	
-	public void fillField(){
-		fillVertical();
-		fillHorizontal();
-		fillDiagonal();
-	}
-	
-	public void fillVertical(){
-		System.out.println("FillVertical");
-		threshold = 2;
-		start = 5;
-		for(int i = 3; i > 0; i--){
-			for(int x = 6; x >= 0; x--){
-				collumn = 0;
-				testcomment = "";
-				for(int y = start; y >= threshold; y--){
-					possibleCombinations[collumn][row] = field[x][y];
-					collumn++;
-					testcomment = testcomment +" "+ x+""+y;
-				}
-				System.out.println(testcomment + " Zeile:"+row);
-				row++;
-			}
-			threshold--;
-			start--;
-		}
-	}
-	
-	public void fillHorizontal(){
-		System.out.println("fillHorizontal");
-		threshold = 3;
-		start = 6;
-		for(int i = 4; i > 0; i--){
-			for(int y = 5; y >= 0 ; y--){
-				collumn = 0;
-				testcomment = "";
-				for(int x = start; x >= threshold; x--){
-					possibleCombinations[collumn][row] = field[x][y];
-					collumn++;
-					testcomment = testcomment +" "+ x+""+y;
-				}
-				System.out.println(testcomment + " Zeile:"+row);
-				row++;
-			}
-			threshold--;
-			start--;
-		}
-	}
-	
-	public void fillDiagonal(){
-		System.out.println("FillDiagonal");
-		for(int y = 5; y >= 3; y--){
-			for(int x = 6; x >= 3; x--){
-				collumn = 0;
-				testcomment = "";
-				for(int i = 4; i > 0; i--){
-					possibleCombinations[collumn][row] = field[x][y];
-					collumn++;
-					testcomment = testcomment +" "+ x+""+y;
-					x--;
-					y--;
-				}
-				x = x + 4;
-				y = y + 4;
-				System.out.println(testcomment + " Zeile:"+row);
-				row++;
-			}
-		}
-		System.out.println("FillDiagonal2");
-		for(int y = 5; y >= 3; y--){
-			for(int x = 0; x <= 3; x++){
-				collumn = 0;
-				testcomment = "";
-				for(int i = 4; i > 0; i--){
-					possibleCombinations[collumn][row] = field[x][y];
-					collumn++;
-					testcomment = testcomment +" "+ x+""+y;
-					x++;
-					y--;
-				}
-				x = x - 4;
-				y = y + 4;
-				System.out.println(testcomment + " Zeile:"+row);
-				row++;
-			}
-		}
 	}
 	
 	public int calculateMove(Move move){
