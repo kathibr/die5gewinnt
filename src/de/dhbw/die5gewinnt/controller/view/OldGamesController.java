c16cd10e2581e9d0e2aa91c71957be510b58fb80
kathibr <kathix3@yahoo.de>
2013-10-02 10:51:43 +0200

    ui update oldGames

--- src/de/dhbw/die5gewinnt/controller/view/OldGamesController.java
+++ src/de/dhbw/die5gewinnt/controller/view/OldGamesController.java
@@ -1,12 +1,19 @@
 package de.dhbw.die5gewinnt.controller.view;
 
+
 import de.dhbw.die5gewinnt.MainApp;
 import de.dhbw.die5gewinnt.controller.db.DBSelects;
 import de.dhbw.die5gewinnt.model.Game;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class OldGamesController {
 
@@ -15,28 +22,75 @@ public class OldGamesController {
 	private Game[] oldGames;
 
 	@FXML
-	private ListView<String> listOfGames;
-	private ObservableList<String> oList;
+	  private TableView<Game> gameTable;
+	@FXML
+	  private TableColumn<Game, String> gameNameColumn;
+	
+	@FXML
+	private ObservableList<Game> oList;
+	@FXML
+	private Label gameName;
+	@FXML
+	private Label playerXName;
+	@FXML
+	private Label playerOName;
+	
+	
 
 	public OldGamesController() {
 		oList = FXCollections.observableArrayList();
 	}
 
 	public void setMainApp(MainApp mainApp) {
-		this.mainApp = mainApp;      
+		this.mainApp = mainApp;
+		loadGameData();     
 	}
 
 	@FXML
 	private void initialize() {
-		loadGameData();
+		gameNameColumn.setCellValueFactory(new PropertyValueFactory<Game, String>("name"));
+		
+		showGameDetails(null);
+		
+		gameTable.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Game>(){
+
+		
+			@Override
+			public void changed(ObservableValue<? extends Game> observable,
+					Game oldValue, Game newValue) {
+				showGameDetails(newValue);				
+			}	
+			
+		});;
+		
+		
+		
+	     
+	      
+		
+		
 	}
 
+	private void showGameDetails(Game game){
+		if(game==null){
+			gameName.setText("");
+			playerOName.setText("");
+			playerXName.setText("");
+		}else{
+			gameName.setText(game.getName());
+		//	playerOName.setText(game.getPlayer());
+//			playerXName.setText(value);
+		}
+		
+	}
 	private void loadGameData() {
+	
 		oldGames = DBSelects.selectGames(true);
 		for(int i = 0; i < oldGames.length; i++){
-			oList.add(oldGames[i].getName());
+			oList.add(oldGames[i]);
 		}
-		listOfGames.setItems(oList);		
+		gameTable.setItems(oList);
+		
 	}
 
 }
