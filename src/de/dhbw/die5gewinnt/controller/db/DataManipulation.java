372ad1e427e3bbaeab7f7b08962745d72f3faba2
Christian <Christian@141.72.146.83>
2013-10-11 11:13:06 +0200

    getSetFieldForDB() doesn't work anymore..wait for bug fixing.

--- src/de/dhbw/die5gewinnt/controller/db/DataManipulation.java
+++ src/de/dhbw/die5gewinnt/controller/db/DataManipulation.java
@@ -40,8 +40,12 @@ public class DataManipulation {
 	public static String getSetFieldForDB(Move[][] field) {
 		String returnField = "";
 		for(int i = 0; i < 7; i++)
-			for(int j = 0; j < 6; j++)
-				returnField += field[i][j].getId()+",";
+			for(int j = 0; j < 6; j++) {
+				if(field[i][j] == null)
+					returnField += "0,";
+				else
+					returnField += field[i][j].getId()+",";
+			}
 		return returnField.substring(0, returnField.length() - 1);
 	}
 	
